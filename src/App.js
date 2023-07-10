1ae41b71b254ec57c1588dbab91fa6e8fdc7c83a
rkeshatwar <rushabhkeshatwar17@gmail.com>
2023-07-10 17:49:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import Cart from './Components/Cart'
